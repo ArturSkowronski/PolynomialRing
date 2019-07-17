6f17b45f376f666c4434a349c70d38c2b1257092
Mikolaj Pabiszczak <mikolajpabiszczak@users.noreply.github.com>
2019-07-17 19:32:51 +0200

    parametric type working

--- tmp.jl
+++ tmp.jl
@@ -1,13 +1,27 @@
 module Tmp
 
 
-
 export Poly
 
-struct Poly
-    a::Vector
+# https://discourse.julialang.org/t/whats-the-correct-way-of-defining-struct-with-an-integer-parameter/23974
+
+# automatic conversion of vector takes place
+# passing empty vector yields error as there is a need to specify Type<:Number
+
+struct Poly{T}
+    a::Vector{T}
+    function Poly(a::AbstractVector{T}) where {T<:Number}
+        if length(a) == 0
+            return new{T}([0])
+        else
+            return new{T}(a)
+        end
+    end
 end
 
 Poly(n::Number) = Poly([n])
 
+
 end
+
+# include("tmp.jl")
