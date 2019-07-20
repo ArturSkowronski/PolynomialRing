75d00ca387498b54acf7acd6371022f969ba1e3d
Mikolaj Pabiszczak <mikolajpabiszczak@users.noreply.github.com>
2019-07-20 17:33:15 +0200

    Fixes; tests &  addition of number added

---
+++ tests.jl
@@ -0,0 +1,74 @@
+include("tmp.jl")
+
+import .Tmp
+using Test
+
+@testset "defining polynomials" begin
+    @test Tmp.Poly(1) == Tmp.Poly([1])
+    @test Tmp.Poly(Int32(1)) == Tmp.Poly([1])
+    @test Tmp.Poly(Int32(1)) == Tmp.Poly(Int64[1]) # as [1] == [1.0]
+    @test Tmp.Poly(1) == Tmp.Poly([1, 0, 0, 0])
+    @test Tmp.Poly([1.0, 2, 3]) == Tmp.Poly([1, 2, 3])
+    @test Tmp.Poly{Int32}([1.0]) == Tmp.Poly(1)
+end
+
+@testset "polynomial evaluation" begin
+    pEmpty = Tmp.Poly(Vector{Int32}([]))
+    p0 = Tmp.Poly(1)
+    p1 = Tmp.Poly([0,1])
+    p2 = Tmp.Poly([0,0,1])
+    p3 = Tmp.Poly([1,1,1])
+    p4 = Tmp.Poly([1,1])
+    p5 = Tmp.Poly([0,0,0,0,2,1])
+
+    @test Tmp.peval(p0, 0) == 1
+    @test Tmp.peval(p0, 2) == 1
+    @test Tmp.peval(p1, 0) == 0
+    @test Tmp.peval(p1, 1//1) == 1
+    @test Tmp.peval(p1, 2) == 2
+    @test Tmp.peval(p1, 3) == 3
+    @test Tmp.peval(p2, 0) == 0
+    @test Tmp.peval(p2, 1) == 1
+    @test Tmp.peval(p2, 2) == 4
+    @test Tmp.peval(p2, √2) ≈ 2
+    @test Tmp.peval(p2, 3.0) == 9
+    @test Tmp.peval(p3, 0) == 1
+    @test Tmp.peval(p3, 1.0) == 3
+    @test Tmp.peval(p3, 2) == 7
+    @test Tmp.peval(p3, 3) == 13
+    @test Tmp.peval(p4, -1) == 0
+    @test Tmp.peval(p5, -1) == 1
+    @test Tmp.peval(p5, 1) == 3
+    @test Tmp.peval(p5, 2) == 64
+    @test Tmp.peval(p5, -2.0) == 0
+    @test Tmp.peval(pEmpty, -1) == 0
+    @test Tmp.peval(pEmpty, 4) == 0
+    @test Tmp.peval(pEmpty, 2//4) == 0
+end
+
+@testset "polynomial addition" begin
+    pEmpty = Tmp.Poly(Vector{Int32}([]))
+    p0 = Tmp.Poly(1)
+    p1 = Tmp.Poly([0,1])
+    p2 = Tmp.Poly([0,0,1])
+    p3 = Tmp.Poly([1,1,1])
+    p4 = Tmp.Poly([1,1])
+    p5 = Tmp.Poly([0,0,0,0,2,1])
+
+    @test pEmpty + 1 == Tmp.Poly(1)
+    @test p0 + 1 == Tmp.Poly(2)
+    @test p1 + 1 == Tmp.Poly([1,1])
+    @test p2 + 1 == Tmp.Poly([1,0,1])
+    @test p3 + 1 == Tmp.Poly([2,1,1])
+    @test p4 + 24.1 == Tmp.Poly([25.1, 1])
+    @test p5 + 3//4 == Tmp.Poly([3//4, 0,0,0,2,1])
+
+
+    @test 1 + pEmpty == Tmp.Poly(1)
+    @test 1 + p0 == Tmp.Poly(2) # sth wrong
+    @test 2 + p1 == Tmp.Poly([2,1])
+    @test 1 + p2 == Tmp.Poly([1,0,1])
+    @test 1 + p3 == Tmp.Poly([2,1,1])
+    @test 24.1 + p4 == Tmp.Poly([25.1, 1])
+    @test 3//4 + p5 == Tmp.Poly([3//4, 0,0,0,2,1])
+end
--- tmp.jl
+++ tmp.jl
@@ -2,6 +2,9 @@ module Tmp
 
 
 export Poly
+export peval
+
+import Base: ==, +
 
 # https://discourse.julialang.org/t/whats-the-correct-way-of-defining-struct-with-an-integer-parameter/23974
 
@@ -9,18 +12,54 @@ export Poly
 # passing empty vector yields error as there is a need to specify Type<:Number
 
 struct Poly{T}
-    a::Vector{T}
-    function Poly(a::AbstractVector{T}) where {T<:Number}
-        if length(a) == 0
+    coeffs::Vector{T}
+    function Poly(coeffs::AbstractVector{T}) where {T<:Number}
+        if length(coeffs) == 0
             return new{T}(zeros(T, 1))
         else
-            return new{T}(a)
+            last_nonz = findlast(!iszero, coeffs)
+            return new{T}(last_nonz == nothing ? coeffs[1] : coeffs[1:last_nonz])
         end
     end
 end
 
-Poly(n::Number) = Poly([n])
+Poly(n::Number) = Poly([n])  # convert number into constant poly
+
+# Promoting type if the type of Poly and Vector do not match
+function Poly{T}(x::AbstractVector{S}) where {T, S}
+    U = promote_type(T, S)
+    return Poly(convert(Vector{U}, x))
+end
+
+# range object of form: start:step:stop
+
+function peval(p::Poly{T}, n::S) where {T, S<:Number}
+    # What should be returned if we pass empty Poly??
+    len = length(p.coeffs)
+    U = promote_type(T, S)
+    b = convert(U, p.coeffs[end])
+    for i in (len-1):-1:1
+        b = p.coeffs[i] + b*n
+    end
+    return b
+end
+
+# Standard functions overloading
+==(p1::Poly, p2::Poly) = (p1.coeffs == p2.coeffs)
+# Question: should 2 == Poly(2) ?
+
+function +(p::Poly{T}, n::S) where {T, S<:Number}
+    U = promote_type(T, S)
+    coeffs1 = T==S ? copy(p.coeffs) : convert(Vector{U}, p.coeffs)
+    coeffs1[1] += n
+    return Poly(coeffs1)
+end
+
++(n::Number, p::Poly) = +(p, n)
+
+
+
+
 
 
 end
-
